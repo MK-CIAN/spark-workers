cdbc8d9362cb98905eef64a9f913243993e7d3d6
Cian McKenna <120397906+MK-CIAN@users.noreply.github.com>
2023-12-13 00:32:05 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,57 +1,63 @@
-from flask import Flask, request
+from flask import Flask
+from flask import request
+from flask import render_template
 import requests
 import os
 import json
-
+from google.cloud import secretmanager_v1
 app = Flask(__name__)
 
 def get_api_key() -> str:
-    secret = os.environ.get("COMPUTE_API_KEY")
-    if secret:
-        return secret
-    else:
-        # Local testing
-        with open('.key') as f:
-            return f.read()
+    # secret = os.environ.get("compute-api-key")
+    project_id = "even-trainer-401512"
+    secret_id = "compute-api-key"
+    
+    client = secretmanager_v1.SecretManagerServiceClient()
+    
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
+    response = client.access_secret_version(name=name)
+    
+    return response.payload.data.decode("UTF-8")
+    # if secret:
+    #     return secret
+    # else:
+    #     #local testing
+    #     with open('.key') as f:
+    #         return f.read()
       
 @app.route("/")
 def hello():
-    return "Add workers to the Spark cluster with a POST request to add"
+    return render_template('index.html')
 
 @app.route("/test")
 def test():
-    return get_api_key()
+    #return "Test" # testing 
+    return(get_api_key())
 
-@app.route("/add", methods=['GET', 'POST'])
+@app.route("/add",methods=['GET','POST'])
 def add():
-    if request.method == 'GET':
-        return "Use post to add"  # Replace with form template
-    else:
-        token = get_api_key()
-        ret = addWorker(token, request.form['num'])
-        return ret
+  if request.method=='GET':
+    return "Use post to add" # replace with form template
+  else:
+    token=get_api_key()
+    ret = addWorker(token,request.form['num'])
+    return ret
+
 
 def addWorker(token, num):
     with open('payload.json') as p:
-        tdata = json.load(p)
-    tdata['name'] = 'slave' + str(num)
-    data = json.dumps(tdata)
-    url = 'https://www.googleapis.com/compute/v1/projects/even-trainer-401512/zones/europe-west1-b/instances'
+        tdata =json.load(p)
+    tdata['name']='slave'+str(num)
+    data=json.dumps(tdata)
+    url='https://www.googleapis.com/compute/v1/projects/even-trainer-401512/zones/europe-west1-b/instances'
     
-    # Encoding and setting the Authorization header with the Bearer token
-    token = token.strip()  # Removing possible leading/trailing spaces or newlines
-    encoded_token = token.encode('utf-8')  # Encode the token as UTF-8 bytes
-    headers = {
-        'Authorization': f'Bearer {encoded_token.decode()}'  # Decode the bytes to string
-    }
-
-    resp = requests.post(url, headers=headers, data=data)
-    
-    if resp.status_code == 200:     
-        return "Done"
+   headers={"Authorization": "Bearer "+token}
+    resp=requests.post(url,headers=headers, data=data)
+    if resp.status_code==200:     
+      return "Done"
     else:
-        print(resp.content)
-        return "Error\n" + resp.content.decode('utf-8') + '\n\n\n' + data
+      print(resp.content)
+      return "Error\n"+resp.content.decode('utf-8') + '\n\n\n'+data
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
