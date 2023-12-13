6f6f188294a1977935ebcb97343e6196d4c4765a
Cian McKenna <120397906+MK-CIAN@users.noreply.github.com>
2023-12-13 13:04:16 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -3,30 +3,21 @@ from flask import request
 import requests
 import os
 import json
-from google.cloud import secretmanager_v1
 app = Flask(__name__)
 
 def get_api_key() -> str:
-    # secret = os.environ.get("compute-api-key")
-    project_id = "even-trainer-401512"
-    secret_id = "compute-api-key"
+    secret = os.environ.get("compute-api-key ")
     
-    client = secretmanager_v1.SecretManagerServiceClient()
-    
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
-    response = client.access_secret_version(name=name)
-    
-    return response.payload.data.decode("UTF-8")
-    # if secret:
-    #     return secret
-    # else:
-    #     #local testing
-    #     with open('.key') as f:
-    #         return f.read()
+    if secret:
+        return secret
+    else:
+        #local testing
+        with open('.key') as f:
+            return f.read()
       
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return "Add workers to the Spark cluster with a POST request to add"
 
 @app.route("/test")
 def test():
@@ -38,13 +29,12 @@ def add():
   if request.method=='GET':
     return "Use post to add" # replace with form template
   else:
-    token=get_api_key()
+    token= get_api_key()
     ret = addWorker(token,request.form['num'])
     return ret
 
-
 def addWorker(token, num):
-     with open('payload.json') as p:
+    with open('payload.json') as p:
       tdata=json.load(p)
     tdata['name']='slave'+str(num)
     data=json.dumps(tdata)
@@ -58,5 +48,6 @@ def addWorker(token, num):
       return "Error\n"+resp.content.decode('utf-8') + '\n\n\n'+data
 
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8080')
+    app.run(host='0.0.0.0',port='8080')
