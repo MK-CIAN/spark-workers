e729f0883b98822e57c07dadeacd8d03facb5166
Cian McKenna <120397906+MK-CIAN@users.noreply.github.com>
2023-12-13 14:47:24 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -3,10 +3,16 @@ from flask import request
 import requests
 import os
 import json
-from google.cloud import secretmanager
 app = Flask(__name__)
 
 def get_api_key() -> str:
+    secret = os.environ.get("compute-api-key")
+    if secret:
+        return secret
+    else:
+        #local testing
+        with open('.key') as f:
+            return f.read()
     #secret = os.environ.get("compute-api-key ")
     #project_id = "655129271851"
     #secret_id = "compute-api-key"
@@ -16,7 +22,7 @@ def get_api_key() -> str:
     #name = "projects/655129271851/secrets/compute-api-key/versions/1"
     #response = client.access_secret_version(name=name)
     
-    return "ya29.a0AfB_byA0FtIs6QpyjW8mhXuStxsVedK1j2Pz0sGMi93f2wO1VtIrawq_kG-qWDaVCw4XAurmxE3Ot680xVjXoAF5VDMk1sEZoJaZGUDTvL5rqRvbwahbSaf9GWPQIClxICjgg-QVvPW6sQCkaPrKQxMwFQ_Xv-LlOzR314BKC9FrHju31douPfpu30oNFpVAMxQkLjtbdETcJTMjXKZU2d2QeC8oxQ0Jbh0DvpmDtim1c0lGEhPgjdp-2CoHicMbfHqqxdeUxxqL9g-sp4QVEevMCiZeqZRfF_diWhDcYGhTVKMyoODN-vBnbqYMC64ePEFVHu9X2DuG10ItQDcy_mBF5bOG21o1rtOGig3cYPHTPRcA1-uUQt6gjinhMX9ntvtVy_x69PQK-Sm6leSpFlgK8hOlH0EaCgYKASwSARMSFQHGX2MizTFRA9z9x7fiv41yt1QO_Q0422"
+    
     # if secret:
     #     return secret
     # else:
@@ -38,7 +44,8 @@ def add():
   if request.method=='GET':
     return "Use post to add" # replace with form template
   else:
-    token="ya29.a0AfB_byA0FtIs6QpyjW8mhXuStxsVedK1j2Pz0sGMi93f2wO1VtIrawq_kG-qWDaVCw4XAurmxE3Ot680xVjXoAF5VDMk1sEZoJaZGUDTvL5rqRvbwahbSaf9GWPQIClxICjgg-QVvPW6sQCkaPrKQxMwFQ_Xv-LlOzR314BKC9FrHju31douPfpu30oNFpVAMxQkLjtbdETcJTMjXKZU2d2QeC8oxQ0Jbh0DvpmDtim1c0lGEhPgjdp-2CoHicMbfHqqxdeUxxqL9g-sp4QVEevMCiZeqZRfF_diWhDcYGhTVKMyoODN-vBnbqYMC64ePEFVHu9X2DuG10ItQDcy_mBF5bOG21o1rtOGig3cYPHTPRcA1-uUQt6gjinhMX9ntvtVy_x69PQK-Sm6leSpFlgK8hOlH0EaCgYKASwSARMSFQHGX2MizTFRA9z9x7fiv41yt1QO_Q0422"
+    token=get_api_key()
+    token=token.strip("\n")
     ret = addWorker(token,request.form['num'])
     return ret
 
